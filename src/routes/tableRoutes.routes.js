e66196d6cc309b2649c58bfb6e9b2f6be66661e5
Wilmer Junior Santiago Donado <jwsantiago@uninorte.edu.co>
2025-10-16 22:58:46 -0500

    Update tableRoutes.routes.js

--- src/routes/tableRoutes.routes.js
+++ src/routes/tableRoutes.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { createTable, getTables, getTableById, updateTable, deleteTable } from "../controllers/table.controller.js";
+import {getTables, getTableById, updateTable, deleteTable } from "../controllers/table.controller.js";
 
 const tableRoutes = Router();
 
-tableRoutes.post("/", createTable);        // Crear mesa
 tableRoutes.get("/", getTables);           // Obtener todas las mesas
 tableRoutes.get("/:id", getTableById);     // Obtener mesa por ID
 tableRoutes.put("/:id", updateTable);      // Actualizar mesa
