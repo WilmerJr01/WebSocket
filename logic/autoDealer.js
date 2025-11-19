549aa55fff5a94bafb300c09900a417555593738
doriada <doriada@uninorte.edu.co>
2025-11-19 14:39:31 -0500

    Update autoDealer.js

--- logic/autoDealer.js
+++ logic/autoDealer.js
@@ -395,7 +395,7 @@ export async function preflop(pre_players, mesa, io, userIdToSocket) {
     mesa.bet = initial_bet + initial_bet / 2
     table.currentHand.pot= initial_bet + initial_bet/2
     await table.save()
-    io.to(mesa.Id).emit("pot:update", table.currentHand.pot);
+    io.to(mesa.Id).emit("pot:update", {pot : table.currentHand.pot});
     console.log("Pot: "+table.currentHand.pot)
     /*
     //empieza a preguntar que hacer a cada jugador ejecutando "turnos"
